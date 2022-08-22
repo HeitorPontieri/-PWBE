ccace6cb40e0d7d6fff68073df1afec48d5774cf
HeitorPontieri <98404376+HeitorPontieri@users.noreply.github.com>
2022-08-22 11:25:33 -0300

    arrayINarraay😶‍🌫️👺

--- aula10/app.js
+++ aula10/app.js
@@ -3,7 +3,8 @@
 // Define uma variavel do tipo array
 const listaNomes = ['jose', 'maria', 'carlos']
 const listaProdutos = ['teclado', 'branco', 50.870, 10]
-const listaAlunos = ['Heitor','Lucas','Antony','Mel', 'Eduardo', 'Leo', 'Arthur', 'Isabelle', 'Lídia', 'Leleco']
+const listaAlunos = ['Heitor','Lucas','Antony','Mel']
+const listaDisciplina = ['PWBE', 'PWFE', 'DB', 'INDMO']
 
 // Exibindo todos os dados de um array
 console.log(listaNomes)
@@ -35,7 +36,61 @@ for(let cont = 0; cont<qntde; cont++){
 }
 console.log('EXEMPLO USANDO O FOREACH')
 // Exemplo usando o foreach
-
-listaAlunos.forEach(function(item,cont){
-    
+listaAlunos.forEach(function(item){
+    console.log('O aluno da turma DS2M é : '+ item)
 })
+
+// Adiciona novos elementos no final do  array 
+listaAlunos.push('Eduardo', 'Leo', 'Arthur', 'Isabelle', 'Lídia', 'Leleco')
+
+console.log('EXEMPLO ADICIONANDO ELEMENTOS AO ARRAY')
+listaAlunos.forEach(function(item){
+    console.log('O aluno da turma DS2M é : '+ item)
+})
+
+console.log('EXEMPLO REMOVENDO ELEMENTOS AO ARRAY')
+// Remove o ultimo item do array
+listaAlunos.pop()
+listaAlunos.forEach(function(item){
+    console.log('O aluno da turma DS2M é : '+ item)
+})
+
+console.log('EXEMPLO ADICIONANDO ELEMENTOS NO INÍCIO DO ARRAY')
+// Adiciona itens no inicio do array
+listaAlunos.unshift('Tony Little Banana')
+listaAlunos.forEach(function(item){
+    console.log('O aluno da turma DS2M é : '+ item)
+})
+
+console.log('EXEMPLO REMOVENDO ELEMENTOS NO INÍCIO DO ARRAY')
+//Remove o primeiro item do array
+listaAlunos.shift()
+listaAlunos.forEach(function(item){
+    console.log('O aluno da turma DS2M é : '+ item)
+})
+
+//  Pesquisando valores em um Array e retornando sua posicao dentro do array
+let indice = listaAlunos.indexOf('Leo')
+console.log(indice)
+
+// Remove elementos a partir de um indice
+    // remove somente o item escolhido
+    listaAlunos.splice(indice, 1)
+    // remove todos os elementos a partir do escolhido
+    listaAlunos.splice(indice)
+    // remove todos os itens a partir do primeiro até o item escolhido
+    listaAlunos.splice(0,indice+1)
+console.log(listaAlunos)
+
+// Cria uma cópia de um array e guarda os itens em outra array 
+const listaNovaAlunos = listaAlunos.slice()
+console.log(listaNovaAlunos)
+
+listaNovaAlunos.push(listaDisciplina)
+console.log(listaNovaAlunos)
+
+// Exibe o primeiro elemento do array contido dentro da linha 8 do array principal
+console.log(listaNovaAlunos [8])
+
+// Exemplo de como buscar um elemento de um array que está dentro de outro array
+console.log(listaNovaAlunos[8].indexOf('DB'))
