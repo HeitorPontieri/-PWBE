cfa63ce29cd08c9d3687d70e7e1280f1b3faf42a
HeitorPontieri <98404376+HeitorPontieri@users.noreply.github.com>
2022-08-25 08:15:03 -0300

    json

---
+++ aula10/json.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// trabalhando com JSON
+const listaContatos = {
+    nome: 'Heitor Pontieri',
+    idade: 16,
+    email: 'heitorpontieri1@gmail.com',
+    telefone: '11 988577810'
+
+}
+
+
+console.log(listaContatos)
+console.log('Nome do contato : ' + listaContatos.nome)
+console.log('O email cadastrado : ' + listaContatos.email)
