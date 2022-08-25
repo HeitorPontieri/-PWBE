39685f698b233129ad54e699a94b2e0ed77ae58c
HeitorPontieri <98404376+HeitorPontieri@users.noreply.github.com>
2022-08-25 09:27:32 -0300

    Update json.js

--- aula10/json.js
+++ aula10/json.js
@@ -1,13 +1,35 @@
 'use strict'
 
 // trabalhando com JSON
-const listaContatos = {
+const listaContatos = [{
     nome: 'Heitor Pontieri',
     idade: 16,
     email: 'heitorpontieri1@gmail.com',
-    telefone: '11 988577810'
+    telefone: '11 988775638',
+    carros :[{
+        placa : 'FHUEM-3759',
+        modelo : 'Gol Sapão',
+        cor : 'Verde Escuro com neon amarelo embaixo'
 
-}
+    },{
+        placa : 'YTUQJ-666',
+        modelo : 'Saveiro',
+        cor : 'rosa'
+    }]
+
+},{
+    nome: 'Antony Little Banana',
+    idade: 16,
+    email: 'tonybananinha@gmail.com',
+    telefone: '11 991899895'
+    
+},{
+    nome: 'leu',
+    idade: 16,
+    email: 'leupokapika@gmail.com',
+    telefone: '11 996336638'
+}]
+
 
 
 console.log(listaContatos)
@@ -21,3 +43,6 @@ console.log(listaContatos)
 // Apaga elementos ao JSON em execução
 delete(listaContatos.altura)
 console.log(listaContatos)
+
+
+console.log('Placa : ' + listaContatos[0].carros[0].placa)
