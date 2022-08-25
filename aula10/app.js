48332adaec369090c404b1692afcfe19524c07bf
HeitorPontieri <98404376+HeitorPontieri@users.noreply.github.com>
2022-08-25 07:50:49 -0300

    7:50 am cmmt 😴

--- aula10/app.js
+++ aula10/app.js
@@ -3,7 +3,7 @@
 // Define uma variavel do tipo array
 const listaNomes = ['jose', 'maria', 'carlos']
 const listaProdutos = ['teclado', 'branco', 50.870, 10]
-const listaAlunos = ['Heitor','Lucas','Antony','Mel']
+const listaAlunos = ['Heitor', 'Lucas', 'Antony', 'Mel']
 const listaDisciplina = ['PWBE', 'PWFE', 'DB', 'INDMO']
 
 // Exibindo todos os dados de um array
@@ -11,9 +11,9 @@ console.log(listaNomes)
 console.log(listaProdutos)
 
 
-console.log(typeof(listaProdutos))
+console.log(typeof (listaProdutos))
 
-console.log(typeof(listaProdutos[3]))
+console.log(typeof (listaProdutos[3]))
 console.log('O produto é :' + listaProdutos[1])
 
 //Exibe a quantidade de elementos de um array 
@@ -25,48 +25,48 @@ let cont = 0
 
 console.log('EXEMPLO USANDO O WHILE')
 //Exemplo usando o while
-while(cont < qntde){
-    console.log('O aluno da turma DS2M é : '+ listaAlunos[cont])
-    cont+=1
+while (cont < qntde) {
+    console.log('O aluno da turma DS2M é : ' + listaAlunos[cont])
+    cont += 1
 }
 console.log('EXEMPLO USANDO O FOR')
 // Exemplo usando o for
-for(let cont = 0; cont<qntde; cont++){
-    console.log('O aluno da turma DS2M é : '+ listaAlunos[cont])
+for (let cont = 0; cont < qntde; cont++) {
+    console.log('O aluno da turma DS2M é : ' + listaAlunos[cont])
 }
 console.log('EXEMPLO USANDO O FOREACH')
 // Exemplo usando o foreach
-listaAlunos.forEach(function(item){
-    console.log('O aluno da turma DS2M é : '+ item)
+listaAlunos.forEach(function (item) {
+    console.log('O aluno da turma DS2M é : ' + item)
 })
 
 // Adiciona novos elementos no final do  array 
 listaAlunos.push('Eduardo', 'Leo', 'Arthur', 'Isabelle', 'Lídia', 'Leleco')
 
 console.log('EXEMPLO ADICIONANDO ELEMENTOS AO ARRAY')
-listaAlunos.forEach(function(item){
-    console.log('O aluno da turma DS2M é : '+ item)
+listaAlunos.forEach(function (item) {
+    console.log('O aluno da turma DS2M é : ' + item)
 })
 
 console.log('EXEMPLO REMOVENDO ELEMENTOS AO ARRAY')
 // Remove o ultimo item do array
 listaAlunos.pop()
-listaAlunos.forEach(function(item){
-    console.log('O aluno da turma DS2M é : '+ item)
+listaAlunos.forEach(function (item) {
+    console.log('O aluno da turma DS2M é : ' + item)
 })
 
 console.log('EXEMPLO ADICIONANDO ELEMENTOS NO INÍCIO DO ARRAY')
 // Adiciona itens no inicio do array
 listaAlunos.unshift('Tony Little Banana')
-listaAlunos.forEach(function(item){
-    console.log('O aluno da turma DS2M é : '+ item)
+listaAlunos.forEach(function (item) {
+    console.log('O aluno da turma DS2M é : ' + item)
 })
 
 console.log('EXEMPLO REMOVENDO ELEMENTOS NO INÍCIO DO ARRAY')
 //Remove o primeiro item do array
 listaAlunos.shift()
-listaAlunos.forEach(function(item){
-    console.log('O aluno da turma DS2M é : '+ item)
+listaAlunos.forEach(function (item) {
+    console.log('O aluno da turma DS2M é : ' + item)
 })
 
 //  Pesquisando valores em um Array e retornando sua posicao dentro do array
@@ -74,12 +74,12 @@ let indice = listaAlunos.indexOf('Leo')
 console.log(indice)
 
 // Remove elementos a partir de um indice
-    // remove somente o item escolhido
-    listaAlunos.splice(indice, 1)
-    // remove todos os elementos a partir do escolhido
-    listaAlunos.splice(indice)
-    // remove todos os itens a partir do primeiro até o item escolhido
-    listaAlunos.splice(0,indice+1)
+// remove somente o item escolhido
+listaAlunos.splice(indice, 1)
+// remove todos os elementos a partir do escolhido
+listaAlunos.splice(indice)
+// remove todos os itens a partir do primeiro até o item escolhido
+listaAlunos.splice(0, indice + 1)
 console.log(listaAlunos)
 
 // Cria uma cópia de um array e guarda os itens em outra array 
@@ -90,7 +90,7 @@ listaNovaAlunos.push(listaDisciplina)
 console.log(listaNovaAlunos)
 
 // Exibe o primeiro elemento do array contido dentro da linha 8 do array principal
-console.log(listaNovaAlunos [8])
+console.log(listaNovaAlunos[8])
 
 // Exemplo de como buscar um elemento de um array que está dentro de outro array
-console.log(listaNovaAlunos[8].indexOf('DB'))
+console.log(listaNovaAlunos[8].indexOf('DB'))   
