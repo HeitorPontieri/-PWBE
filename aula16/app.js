f8a3d92bf9e6cd71a4e7334285834862db59e9e6
HeitorPontieri <98404376+HeitorPontieri@users.noreply.github.com>
2022-10-05 07:33:34 -0300

    palmeiras endeca??

---
+++ aula16/app.js
@@ -0,0 +1,11 @@
+
+//npm install express --save
+// npm install cors -- save
+// npm install body-parser --save
+// 
+// Para manipular o acesso a BD podemos utizar o Prisma
+    // Para instalar o prisma, devemos rodar os seguintes comandos
+        // npm install prisma --save
+        // npx prisma
+        // npx init prisma
+        // npm install @prisma/client
